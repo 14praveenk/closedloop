1836586c8ff5d2f6b2b6be2049429cae7d47ccf6
Praveen <16594062+pravsterrr@users.noreply.github.com>
2024-10-14 12:41:23 +0100

    Mobile support

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
-import 'dart:html'; // Import this for localStorage
 import 'dart:io'; // Import this for Platform
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'page1.dart';
-import 'page2NEW.dart';
-import 'page3NEW.dart';
+
+import 'page2NEWMOB.dart' // Stub implementation
+    if (dart.library.io) 'page2NEWMOB.dart' // dart:io implementation
+    if (dart.library.js_interop) 'page2NEWWEB.dart'; // package:web implementation
+
+import 'page3NEWMOB.dart' // Stub implementation
+    if (dart.library.io) 'page3NEWMOB.dart' // dart:io implementation
+    if (dart.library.js_interop) 'page3NEWWEB.dart'; // package:web implementation
+    
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:cuberto_bottom_bar/cuberto_bottom_bar.dart';
 
@@ -34,25 +40,10 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    _checkPromptStatus(); // Check if the prompt has been shown
+
   }
 
-  void _checkPromptStatus() {
-    // Check localStorage for the prompt status
-    _promptShown = window.localStorage['prompt_shown'] == 'true';
 
-    // Check for iOS and show prompt if not shown before
-    if (defaultTargetPlatform == TargetPlatform.iOS && !_promptShown) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _showInstallPrompt();
-      });
-    }
-  }
-
-  void _setPromptShown() {
-    // Set the value of the prompt shown flag to true in localStorage
-    window.localStorage['prompt_shown'] = 'true';
-  }
 
   @override
   void dispose() {
@@ -150,48 +141,4 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void _showInstallPrompt() {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: Color.fromARGB(255, 27, 27, 27),
-      builder: (context) {
-        return Container(height:200,margin:EdgeInsets.fromLTRB(10, 0, 10, 0),child:Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 16),
-              Column(mainAxisAlignment: MainAxisAlignment.center,children:[Text("Install the app on your device to easily access it anytime from your home screen.",textAlign:TextAlign.center,style: TextStyle(color: Colors.white, fontSize: 18))]),SizedBox(height:40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '1. Tap on Share',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),                SizedBox(width: 8),Icon(Icons.ios_share_sharp, color: Colors.white, size: 24),
-                ],
-              ),
-              SizedBox(width: 16),
-              Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 8),
-                  Text(
-                    '2. Select Add to Home Screen',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                   SizedBox(width: 8),
-                  Icon(Icons.home, color: Colors.white, size: 24),
-                ],
-              ),
-              SizedBox(width: 16),
-              IconButton(
-                icon: Icon(Icons.close, color: Colors.white),
-                onPressed: () {
-                _setPromptShown(); // Set the flag when dismissed
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ));
-      },
-    );
-  }
 }
